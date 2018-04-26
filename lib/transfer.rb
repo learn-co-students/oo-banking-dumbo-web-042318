a0417ba3e4635abffad1f8a250bad56d9db698be
jessebillard <jessebillard@mac.com>
2018-04-26 16:25:24 -0400

    OO banking and transfers

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,39 @@
 class Transfer
-  # your code here
+  
+  attr_accessor :sender, :receiver, :amount, :status
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end 
+
+  def valid?
+    if @sender.valid? && @receiver.valid? && @sender.balance > @amount
+      true
+    else
+      false
+    end 
+  end 
+
+  def execute_transaction
+    if self.valid? && self.status == "pending"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      self.status = "complete"
+    else
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end 
+
+  def reverse_transfer
+    if self.status == "complete"
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      self.status = "reversed"
+    end 
+  end 
+
 end
