1efea3957e35de6e582b5f5c2cc9e4392d219efd
ahjuice <ahjuice@users.noreply.github.com>
2018-04-26 15:34:03 -0400

    completed lab

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,30 @@
 class BankAccount
+  attr_accessor :balance, :status
+  attr_reader :name
 
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(amount)
+    #can deposit money into its account
+    self.balance += amount
+  end
+
+  def display_balance
+    #can display its balance
+    "Your balance is $#{self.balance}."
+  end
+
+  def valid?
+    #is valid with an open status and a balance greater than 0
+    self.status == "open" && self.balance > 0
+  end
+
+  def close_account
+    #can close its account
+    self.status = "closed"
+  end
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,42 @@
+require 'pry'
 class Transfer
-  # your code here
+
+  attr_accessor :sender, :receiver, :status, :amount
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    #can check that both accounts are valid (FAILED - 14)
+    #  calls on the sender and reciever's #valid? methods
+    self.sender.valid? && self.receiver.valid?
+  end
+
+  def execute_transaction
+     # can execute a successful transaction between two accounts (FAILED - 16)
+     # each transfer can only happen once (FAILED - 17)
+     # rejects a transfer if the sender doesn't have a valid account
+     if self.valid? && self.status != "complete" && self.amount < self.sender.balance
+       self.sender.balance -= self.amount
+       self.receiver.balance += self.amount
+       self.status = "complete"
+     else
+       self.status = "rejected"
+       "Transaction rejected. Please check your account balance."
+     end
+  end
+
+  def reverse_transfer
+   # can reverse a transfer between two accounts (FAILED - 19)
+   #   it can only reverse executed transfers
+    if self.status == "complete"
+     self.sender.balance += self.amount
+     self.receiver.balance -= self.amount
+     self.status = "reversed"
+   end
+  end
 end
